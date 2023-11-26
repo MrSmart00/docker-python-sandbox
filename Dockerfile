700182ace4ce4f76d9a9664d80a1a9338326a2a9
MrSmart00 <nexearth@gmail.com>
2023-11-26 22:24:30 +0900

    Fix: docker settings

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM python:3
 
-WORKDIR /usr/src/app
-
 COPY . .
 
-CMD [ "python", "./src/main.py" ]
+WORKDIR /src
--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
-import math
-import sys
+import asyncio
+import time
 
-def main():
-  print('Hello, world!')
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(f"{time.strftime('%X')} - {what}")
 
-if __name__ == "__main__":
-  main()
+async def main():
+    print(f"{time.strftime('%X')} - start")
+    c1 = say_after(1, 'hello')
+    c2 = say_after(2, 'world')
+    await asyncio.gather(c1, c2)
+    print(f"{time.strftime('%X')} - finish")
+
+asyncio.run(main())
